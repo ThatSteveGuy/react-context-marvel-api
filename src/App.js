e12010d5a3fb55b9b440baf02b004aaf35f9ded4
ThatSteveGuy <whitaker.se@gmail.com>
2019-01-16 15:55:32 -0600

    missing files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
+import PropogateLoader from 'react-spinners';
+
+import CharacterDisplay from './containers/CharacterDisplay';
+import CharacterPicker from './containers/CharacterPicker';
+
+//const CharacterPicker = lazy(() => import('./containers/CharacterPicker'));
+//onst CharacterDisplay = lazy(() => import('./containers/CharacterDisplay'));
 
 class App extends Component {
   render() {
@@ -8,19 +17,31 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {/* <Suspense fallback={<PropogateLoader size="15" color="red" />}> */}
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={CharacterPicker} />
+              {/* <Suspense
+                fallback={<div>meh...</div>}
+              > */}
+                <Route path="/character/:id" component={CharacterDisplay} />
+              {/* </Suspense> */}
+            </Switch>
+          </BrowserRouter>
+        {/* </Suspense> */}
       </div>
+    );
+  }
+}
+
+// working though why we need this... getting an error that router expect func, not obj
+function FuncWrapper (Component) {
+  return (props) => {
+    return (
+      <Suspense fallback={<PropogateLoader size="15" color="red" />}>
+        <Component {...props} />
+      </Suspense>
     );
   }
 }
